82b8fddea5e3b74f76544dc599ca978f9033d136
Krishan Shamod <krishanshamod@gmail.com>
2023-01-19 10:29:21 +0530

    Added security group configurations

---
+++ security_groups.tf
@@ -0,0 +1,41 @@
+# Define the security group for the jenkins host
+resource "aws_security_group" "jenkins_vm_sg" {
+  name        = "jenkins_vm_sg"
+  description = "Allow incoming connections for Jenkins VM"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming SSH connections"
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming 8080 port connections"
+  }
+
+  ingress {
+    from_port   = 8
+    to_port     = 0
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow ping from anywhere"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "jenkins_vm_sg"
+  }
+}
